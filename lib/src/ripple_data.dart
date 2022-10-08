06b8bebb909db1bcdcc3e0d04c66f5c745214418
Gurkaran Singh <gurkaran@grevity.in>
2022-10-08 06:59:06 +0530

    converted to null safety

--- lib/src/ripple_data.dart
+++ lib/src/ripple_data.dart
@@ -2,22 +2,22 @@ import 'package:flutter/material.dart';
 
 class RippleData {
   RippleData({
-    @required this.context,
-    @required this.color,
-    @required this.animationDuration,
-    @required this.delay,
-    @required this.shape,
-    @required this.inflateMultiplier,
+    required this.context,
+    required this.color,
+    required this.animationDuration,
+    required this.delay,
+    required this.shape,
+    required this.inflateMultiplier,
   });
 
-  final BuildContext context;
-  final Color color;
-  final Duration animationDuration;
-  final Duration delay;
-  final BoxShape shape;
-  final double inflateMultiplier;
+  late BuildContext context;
+  late Color color;
+  late Duration animationDuration;
+  late Duration delay;
+  late BoxShape shape;
+  late double inflateMultiplier;
 
-  Rect rect;
+  late Rect? rect;
 
   Widget toWidget() {
     if (rect == null) {
@@ -25,10 +25,10 @@ class RippleData {
     }
 
     return AnimatedPositioned(
-      left: rect.left,
-      top: rect.top,
-      right: MediaQuery.of(context).size.width - rect.right,
-      bottom: MediaQuery.of(context).size.height - rect.bottom,
+      left: rect!.left,
+      top: rect!.top,
+      right: MediaQuery.of(context).size.width - rect!.right,
+      bottom: MediaQuery.of(context).size.height - rect!.bottom,
       child: Container(
         decoration: BoxDecoration(
           shape: shape,
